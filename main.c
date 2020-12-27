2b94d0df2ad29b7d1acd94eb72f6f26ab31b2ed8
WIll <x_orin1@pop-os.localdomain>
2020-12-27 11:02:51 -0500

    interpolated shader

--- main.c
+++ main.c
@@ -3,17 +3,21 @@
 const char *vertexShaderSource =
   "#version 330 core\n"
   "layout (location = 0) in vec3 aPos;\n"
+  "layout (location = 1) in vec3 aColor;\n"
+  "out vec3 ourColor;\n"
   "void main()\n"
   "{\n"
-  " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+  " gl_Position = vec4(aPos, 1.0);\n"
+  "ourColor = aColor;\n"
   "}\0";
 
 const char *fragmentShaderSource = 
   "#version 330 core\n"
   "out vec4 FragColor;\n"
+  "in vec3 ourColor;\n"
   "void main()\n"
   "{\n"
-  "FragColor = vec4(1.0f,0.5f,0.2f,1.0f);\n"
+  "FragColor = vec4(ourColor, 1.0f);\n"
   "}\0";
 
 
@@ -48,22 +52,25 @@ int main()
     }
   glfwMakeContextCurrent(window);
 
-  glViewport(0,0,800,600);
+  //  glViewport(0,0,800,600);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   //build and compile shader program
   int vertexShader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
   glCompileShader(vertexShader);
+
   //check for compile errors
   int success;
   char infoLog[512];
   glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
   if(!success)
     {
       glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-      printf("Error on Vertex Shader compilation", infoLog);
+      printf("Error on Vertex Shader compilation");
     }
+
   //fragement shader
   int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
@@ -72,7 +79,7 @@ int main()
   if(!success)
     {
       glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-      printf("Error on Fragment Shader Compilation", infoLog);
+      printf("Error on Fragment Shader Compilation");
     }
 
   //Link the shaders
@@ -86,7 +93,7 @@ int main()
   if(!success)
     {
       glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-      printf("Error on Shader linking", infoLog);
+      printf("Error on Shader linking");
     }
 
   glDeleteShader(vertexShader);
@@ -94,25 +101,31 @@ int main()
 
 
   float vertices[] = {
-                      -0.5f, -0.5f, 0.0f,
-                      0.5f, -0.5f, 0.0f,
-                      0.0f, 0.5f, 0.0f
+                      // positions         // colors
+                      0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+                      -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+                      0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top
   };
 
+  //Vertex buffer and array objects -- these are required by openGL
   unsigned int VBO, VAO;
   glGenVertexArrays(1,&VAO);
   glGenBuffers(1,&VBO);
+  //bind the vertex array object first, then bind and set vertex buffers, then configure  vertex attributes
   glBindVertexArray(VAO);
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  //position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
 
-  glBindBuffer(GL_ARRAY_BUFFER,0);
+  //color attribute
+  glVertexAttribPointer(1,3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 *sizeof(float)));
+  glEnableVertexAttribArray(1);
 
-  glBindVertexArray(0);
+  glUseProgram(shaderProgram);
 
   while(!glfwWindowShouldClose(window))
     {
@@ -120,14 +133,14 @@ int main()
 
       glClearColor(0.2f,0.3f,0.3f,1.0f);
       glClear(GL_COLOR_BUFFER_BIT);
-
-      glUseProgram(shaderProgram);
+      
       glBindVertexArray(VAO);
       glDrawArrays(GL_TRIANGLES, 0, 3);
 
       glfwSwapBuffers(window);
       glfwPollEvents();
     }
+
   glDeleteVertexArrays(1,&VAO);
   glDeleteBuffers(1, &VBO);
   glDeleteProgram(shaderProgram);
