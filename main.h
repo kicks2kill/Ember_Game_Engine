09bd04efa6316499f840cea6e2a8c387496f9031
WIll <x_orin1@pop-os.localdomain>
2020-12-26 22:50:39 -0500

    init

---
+++ main.h
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <GL/glut.h>
+
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+void processInput(GLFWwindow *window);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
